c2093d777e12ec1aa6ff20239abd4880a73b4560
Francis Andres <fbandres17@gmail.com>
2022-04-19 19:49:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# About
+
+A collection of Philippine Standard Geographic Code that were broken down by:
+
+- Barangays
+- Cities / Municipalities
+- Provinces
+- Regions
+
+## PSGC Publication Date
+- Q4-2021
+
+## Installation
+```bash
+$ npm install @dctsph/psgc
+```
+
+## References
+- [Philippine Statistics Authority (PSGC)](https://psa.gov.ph/classification/psgc/)
+- [Wikipedia](https://en.wikipedia.org/)
+
+## License
+
+Licensed under the [MIT](https://opensource.org/licenses/MIT) license.
---
+++ index.js
@@ -0,0 +1,53 @@
+import barangays from "./json/barangays.json"
+import municipalities from "./json/municipalities.json"
+import provinces from "./json/provinces.json"
+import regions from "./json/regions.json"
+
+function getBarangaysByMunicipality(mun_code) {
+  const municipalityCode = mun_code.toString()
+  return barangays.filter(value => value.mun_code === municipalityCode)
+}
+
+function getMunicipalitiesByProvince(prv_code) {
+  const provincialCode = prv_code.toString()
+  return municipalities.filter(value => value.prv_code === provincialCode)
+}
+
+function getProvincesByRegion(reg_code) {
+  const regionCode = reg_code.toString()
+  return provinces.filter(value => value.reg_code === regionCode)
+}
+
+function getBarangaysByProvince(prv_code) {
+  const provincialCode = prv_code.toString()
+  const barangaysByProvince = []
+  const provinceMunicipalities = getMunicipalitiesByProvince(provincialCode).filter(value => value.prv_code === provincialCode)
+
+  for (const municipality of provinceMunicipalities) {
+    const filteredBarangays = getBarangaysByMunicipality(municipality.mun_code)
+
+    for (const barangay of filteredBarangays) {
+      barangaysByProvince.push({
+        ...barangay,
+        municipality: municipality.name
+      })
+    }
+  }
+
+  return barangaysByProvince
+}
+
+function getRegionByProvince(prv_code) {
+  const provincialCode = prv_code.toString()
+  const filteredProvince = provinces.find(value => value.prv_code === provincialCode)
+  return regions.filter(value => value.reg_code === filteredProvince.reg_code)
+}
+
+export default {
+  getBarangaysByMunicipality,
+  getMunicipalitiesByProvince,
+  getProvincesByRegion,
+  getBarangaysByProvince,
+  getRegionByProvince,
+  regions
+}
